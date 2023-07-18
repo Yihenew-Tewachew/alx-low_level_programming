51aa717b6e2a6cea267471dc029530079bb8d1e3
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-17 19:52:30 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Prints combinations of hundreds
+ * Return: Always 0
+ */
+int main(void)
+{
+	int one = 0;
+	int ten = 0;
+	int hun = 0;
+
+	for (hun = 0; hun <= 9; hun++)
+	{
+		for (ten = 0; ten <= 9; ten++)
+		{
+			for (one = 0; one <= 9; one++)
+			{
+				if (!((one == ten) || (ten == hun) || (ten > one) || (hun > ten)))
+				{
+					putchar(hun + '0');
+					putchar(ten + '0');
+					putchar(one + '0');
+					if (!(one == 9 && hun == 7 && ten == 8))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
