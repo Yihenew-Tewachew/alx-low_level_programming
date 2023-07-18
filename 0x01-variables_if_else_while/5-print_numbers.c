8d0ce52b5612558b4f7b36fc7fbc8b93248751e1
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-17 18:31:21 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This program prints first digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
