9742dd694f94f6433c48e4f5ebbcc6164444d17c
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-19 17:39:51 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main _ Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int x = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z = z + i;
+		}
+		i++;
+	}
+	printf("%d\n", x);
+	return (0);
+}
