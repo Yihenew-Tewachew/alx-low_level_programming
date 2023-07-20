d75a15b6a408bd3e6413b242a7888c316caa4273
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-20 10:52:51 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - computes the product of two nums
+ * @a: nums to be computed
+ * @b: num to be multiplied
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
