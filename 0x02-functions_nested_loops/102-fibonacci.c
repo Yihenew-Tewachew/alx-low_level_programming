c6ce558b913e6d73667c21bf1a6d660eedb31cae
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-19 18:09:59 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Prints fibonaci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	int k = 2;
+
+	while (a < 50)
+	{
+		if (a == 0)
+			printf("%d", b);
+		else if (a == 1)
+			printf(", %d", c);
+		else
+		{
+			c += b;
+			b = c - b;
+			printf(". %d", c);
+		}
+			++a;
+		}
+		printf("\n");
+		return (0);
+}
