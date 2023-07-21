30628aa61786a324cce670e00e11b8da9d4bd06d
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-20 18:57:07 -0700

    ./8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - prints square hastags
+ * @size: the number of ows or columns
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
