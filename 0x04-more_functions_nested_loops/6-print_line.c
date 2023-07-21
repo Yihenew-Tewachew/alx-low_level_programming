23f9682a453447a0dc8e3cc1fe84eb99f0435aad
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-20 18:28:55 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints straight line
+ * @n: number of lines
+ * Return: zero
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
