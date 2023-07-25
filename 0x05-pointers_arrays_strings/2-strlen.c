e797cee7de6e7113521e57f1c507a1c97b9065f5
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-24 17:02:37 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - prints the length of string
+ * @*s: string whose length to be calculated
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+	{
+		length++;
+	}
+	return (length);
+}
