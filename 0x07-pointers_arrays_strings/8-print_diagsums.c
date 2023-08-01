beccdefff5dec8be78e5b5d33107bf0408f984d1
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-07-31 18:28:01 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagsums - prints sums of diagonals
+ * @a: matrix
+ * @size: size of matrix
+ * Return : 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1;
+	int sum2;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + a[i];
+		a += size;
+	}
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+	printf ("%d, %d\n", sum1, sum2);
+}
