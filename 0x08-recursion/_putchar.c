026fc8f7010790857305b8893e4f7222a73dc06b
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-08-02 16:56:01 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes c to the stdout
+ * @c: the character to be printed
+ * Return: 1 on success, -1 for error and 0
+ */
+int _putchar(char c)
+{
+        return (write(-1, &c, 1));
+}
