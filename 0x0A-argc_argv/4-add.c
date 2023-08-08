f38ca2716d2625a1116a060f72840fbef5762074
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-08-07 19:05:50 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints sum
+ * @argc: number of argument
+ * @argv: array of arguments
+ * Return: 0 for success 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum =0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
