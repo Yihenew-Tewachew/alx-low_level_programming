4d012697ad11fedd3cad4b5eb12a752e17a7442d
Yihenew-Tewachew <yihenewtewachew@gmail.com>
2023-08-09 17:41:00 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *argstostr - concatenates the arguments of the program
+ * @ac: number of arguments
+ * @av: argument vevtor
+ * Return: a pointer or 0
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str, *s;
+	int i, j, k, len = 0;
+
+	if (ac == 0 && av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		j = 0;
+		while (s[j++])
+			len++;
+		len++;
+	}
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0, j = 0; i < ac && j < len; i++)
+	{
+		s = av[i];
+		k = 0;
+		while (s[k])
+		{
+			str[j] = s[k];
+			k++;
+			j++;
+		}
+		str[j++] = ('\n');
+	}
+	str[j] = ('\0');
+	return (str);
+}
